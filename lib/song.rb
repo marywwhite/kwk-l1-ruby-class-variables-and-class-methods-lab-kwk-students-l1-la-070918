46585a71f59c6358223981be6dc951acdbebc23b
Mary White <maryewhite2022@gmail.com>
2018-07-13 18:52:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,26 @@
+class Song
 
+  @@song_count = 0
+  def initialize
+    @@song_count += 1
+  end
+  def self.song_count
+    @@song_count
+  end
+  
+  Song.artists
+  Song.genres
+  Song.genres_count
+  Song.artist_count
+  
+  ninety_nine_problems = 
+  Song.new("99 Problems","Jay-z","rap")
+  
+  ninety_nine_problems.name
+  ninety_nine_problems.artist
+  ninety_nine_problems.genre
+  
+  
+end
+
+Song.count
