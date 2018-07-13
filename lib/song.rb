6e3562fb14da2d7ce18d2f371422232e54333215
Mary White <maryewhite2022@gmail.com>
2018-07-13 19:02:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,25 @@ class Song
    @@artists << artist
  end
   
-  
-  if artist_count{artist} +=1
-  else
-    
-    
-  end
+  def self.count
+   @@count
+ end
+
+ def self.artists
+   @@artists.uniq
+ end
+
+ def self.artist_count
+   artist_count = { }
+   @@artists.each do |artist|
+     if artist_count[artist]
+       artist_count[artist] += 1
+     else
+       artist_count[artist] = 1
+     end
+   end
+   artist_count
+ end
   
   def self.genres
     @@genres.uniq
