8699d015dcf3f973ccb39c67ebcfa7ef2510a72e
Mary White <maryewhite2022@gmail.com>
2018-07-13 18:57:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,20 @@
 class Song
 
-  @@song_count = 0
-  def initialize
-    @@song_count += 1
-  end
-  def self.song_count
-    @@song_count
-  end
-  
-  Song.artists
-  Song.genres
-  Song.genres_count
-  Song.artist_count
-  
-  ninety_nine_problems = 
-  Song.new("99 Problems","Jay-z","rap")
-  
-  ninety_nine_problems.name
-  ninety_nine_problems.artist
-  ninety_nine_problems.genre
+  attr_accesor :name, :artist, :assert_generates
+  @@count = 0
+  @@artists = []
   
   
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] +=1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
 end
 
 Song.count
